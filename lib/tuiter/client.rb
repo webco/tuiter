815b4710c5e28a20181c1817412774d6f6eb2521
Thomaz Leite <thomaz@leite.me>
2009-04-15 09:49:38 -0500

    friendship methods moved to module

--- lib/tuiter/client.rb
+++ lib/tuiter/client.rb
@@ -1,7 +1,11 @@
 module Tuiter
 
+  require 'tuiter/methods/friendships'
+
   class Client
     attr_accessor :username, :password
+    
+    include FriendshipMethods
     
     def initialize(options = {})
       @pid = Process.pid
@@ -20,7 +24,7 @@ module Tuiter
       req.set_form_data({'status'=>status, 'in_reply_to_status_id'=>in_reply_to_status_id })
       res = new_http_for(url).start {|http| http.request(req) }
       case res
-      when Net::HTTPSuccess, Net::HTTPRedirection
+      when Net::HTTPSuccess, Net::HTTPReFion
         log("update() success: OK")
         return res # OK
       else
@@ -58,39 +62,6 @@ module Tuiter
       end
     end
 
-    def friendship_new(user, follow = nil)
-      log("friendship_new() following: #{user}")
-      url = URI.parse("http://twitter.com/friendships/create/#{user}.json")
-      req = Net::HTTP::Post.new(url.path)
-      req.basic_auth @username, @password
-      req.set_form_data({'follow'=>"true"}) if follow
-      res = new_http_for(url).start {|http| http.request(req) }
-      case res
-      when Net::HTTPSuccess, Net::HTTPRedirection
-        log("friendship_new() success: OK")
-        return res # OK
-      else
-        log("friendship_new() error: #{res.error!}")
-        res.error!
-      end
-    end
-
-    def remove_friendship(user, follow = nil)
-      log("friendship_new() following: #{user}")
-      url = URI.parse("http://twitter.com/friendships/destroy/#{user}.json")
-      req = Net::HTTP::Post.new(url.path)
-      req.basic_auth @username, @password
-      res = new_http_for(url).start {|http| http.request(req) }
-      case res
-      when Net::HTTPSuccess, Net::HTTPRedirection
-        log("remove_friendship() success: OK")
-        return res # OK
-      else
-        log("remove_friendship() error: #{res.error!}")
-        res.error!
-      end
-    end
-    
     def verify_credentials?
       if res = request("http://twitter.com/account/verify_credentials.json")
         return UserExtended.new(JSON.parse(res))
@@ -196,15 +167,6 @@ module Tuiter
     def rate_limit
       if res = request("http://twitter.com/account/rate_limit_status.json")
         return RateLimit.new(JSON.parse(res))
-      else
-        return nil
-      end
-    end
-    
-    def follows_me?(id)
-      if res = request("http://twitter.com/friendships/exists.json?user_a=#{id}&user_b=#{@username}")
-        return true if res == "true"
-        return false
       else
         return nil
       end
