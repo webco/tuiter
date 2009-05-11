67a92d892fb52c945400a28fc14a326f5b658419
Luiz Rocha <lsdrocha@gmail.com>
2009-05-11 11:00:17 -0300

    covering remaining direct_message API call

--- lib/tuiter/methods/direct_message.rb
+++ lib/tuiter/methods/direct_message.rb
@@ -2,7 +2,7 @@
 # [X] direct_messages
 # [X] direct_messages/sent
 # [X] direct_messages/new
-# [ ] direct_messages/destroy
+# [X] direct_messages/destroy
 
 module Tuiter
 
@@ -33,18 +33,32 @@ module Tuiter
     end
 
     def direct_messages_new(user, text)
-      log("direct_new() sending: #{text} to #{user}")      
+      log("direct_messages_new() sending: #{text} to #{user}")      
       res = @request_handler.post('/direct_messages/new.json', {'user'=>user, 'text'=>text })
+
       case res
       when Net::HTTPSuccess, Net::HTTPRedirection
-        log("direct_new() success: OK")
-        return res # OK
+        log("direct_messages_new() success: OK")
+        return Tuiter::DirectMessage.new(JSON.parse(res.body))
       else
-        log("direct_new() error: #{res.error!}")
+        log("direct_messages_new() error: #{res.error!}")
         res.error!
       end
     end
   
+    def direct_messages_destroy(id)
+      log("direct_messages_destroy(): #{id}")
+      res = @request_handler.delete("/direct_messages/destroy/#{id}.json")
+
+      case res
+      when Net::HTTPSuccess
+        log("direct_messages_destroy() #{id} success: OK")
+        return Tuiter::DirectMessage.new(JSON.parse(res.body))
+      else
+        log("direct_messages_destroy() #{id} error: #{res.error!}")
+        res.error!
+      end
+    end
   end
 
 end
