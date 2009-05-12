0a552d4f3230d33fe19ef3355f106e2e5dd0089a
Luiz Rocha <lsdrocha@gmail.com>
2009-05-12 20:31:51 -0300

    initial work in user_timeline

--- lib/tuiter/methods/status.rb
+++ lib/tuiter/methods/status.rb
@@ -11,6 +11,32 @@ module Tuiter
 
   module StatusMethods
     
+    def statuses_user_timeline(id = nil, options = {})
+      if id.kind_of? Hash
+        options.merge(id)
+        id = username
+      end
+
+      options.delete(:user_id) if options.has_key? :screen_name
+      id ||= username
+      
+      unless (options[:screen_name] || options[:user_id])
+        url = "/statuses/user_timeline/#{id}.json"
+      else
+        url = "/statuses/user_timeline.json"
+      end
+      
+      params = parse_options(options) || ""
+      res = @request_handler.get(url+params)
+
+      if res
+        data = JSON.parse(res.body)
+        return data.map { |d| Tuiter::Status.new(d) }
+      else
+        res.error!
+      end
+    end
+
     def statuses_show(id)
       if res = @request_handler.get("/statuses/show/#{id}.json").body
         return Tuiter::Status.new(JSON.parse(res))
